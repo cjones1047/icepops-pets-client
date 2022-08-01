8fc488eeef88f89739cb27c9b4838cdd18815d2d
cjones1047 <cjmj1047@gmail.com>
2022-08-01 14:25:59 -0500

    Toy API functions added

---
+++ src/api/toys.js
@@ -0,0 +1,44 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+// CREATE
+export const createToy = (user, newToy) => {
+    console.log('createToy in API was hit')
+    console.log('this is user in createToy', user)
+    console.log('this is newToy in createToy', newToy)
+	return axios({
+		url: `${apiUrl}/toys/${petId}`,
+		method: 'POST',
+		data: {
+			toy: newToy,
+		},
+	})
+}
+
+// UPDATE
+export const updateToy = (user, petId, updatedToy) => {
+    console.log('updateToy in API was hit')
+    console.log('this is updatedToy', updatedToy)
+	return axios({
+		url: `${apiUrl}/toys/${petId}/${updatedToy.id}`,
+		method: 'PATCH',
+		headers: {
+			Authorization: `Token token=${user.token}`
+		},
+		data: {
+			toy: updatedToy
+		}
+	})
+}
+
+// DELETE
+export const deleteToy = (user, petId, toyId) => {
+    console.log('deleteToy in API was hit')
+	return axios({
+		url:`${apiUrl}/pets/${petId}/${toyId}`,
+		method: 'DELETE',
+		headers: {
+			Authorization: `Token token=${user.token}`
+		}
+	})
+}
